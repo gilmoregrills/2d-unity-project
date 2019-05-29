1d76c9655cc3a4c86aac60d31ec909c5ddf7d426
Robin Yonge <robinfarrowyonge@gmail.com>
2019-05-29 15:34:09 +0100

    first commit, scripts and placeholder stuff

---
+++ Scripts/GrassSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassSpawner : MonoBehaviour
+{
+
+    [Range( 0, 200 )]
+    public int grassDensity = 20;
+
+    private GameObject grassStarter;
+
+    private Transform grassParent;
+
+    private GameObject[] ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grassStarter = GameObject.Find("Grass");
+        grassParent = grassStarter.transform.parent;
+        ground = GameObject.FindGameObjectsWithTag("ground");
+
+        Debug.Log("Generating " + grassDensity + " pieces of grass");
+
+        for (int i = 0; i < grassDensity; i++)
+        {
+            Vector3 newPosition = RandomPosition(grassStarter.transform.position, i);
+            grassStarter = Instantiate(grassStarter, newPosition, Quaternion.identity, grassParent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Called by instantiate to generate a random grass position
+    // Should choose a position where:
+    //     Y is equal to the top boundary of a ground piece
+    //     X is not within a range of another grass piece
+    // Taking the position of the last blade of grass and generating
+    // a new position that is NOT that should help to keep things
+    // distributed
+    Vector2 RandomPosition(Vector3 lastPosition, int seed)
+    {
+        GameObject platform = ground[Random.Range(0, ground.Length)];
+        Vector3 platformPosition = platform.transform.position;
+        Vector3 platformScale = platform.transform.localScale;
+        Debug.Log(platformPosition);
+        platformPosition.y += 0.2f;
+        platformPosition.x = Random.Range(platformPosition.x - platformScale.x / 2 + 0.1f, platformPosition.x + platformScale.x / 2 - 0.1f);
+        return platformPosition;
+    }
+}
---
+++ Scripts/GroundSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+
+    public Vector3 worldDimensions;
+
+    [Range( 0, 200 )]
+    public int totalGround;
+
+    private GameObject groundStarter;
+
+    private Transform groundParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundStarter = GameObject.Find("Ground");
+        groundParent = groundStarter.transform.parent;
+
+        Debug.Log("Generating " + totalGround + "units of ground areas");
+
+        for (int i = 0; i < totalGround; i++)
+        {
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Called by instantiate to generate a random ground position
+    // Should choose a position where:
+    //     Y is equal to the top boundary of a ground piece
+    //     X is not within a range of another ground piece
+    // Taking the position of the last blade of ground and generating
+    // a new position that is NOT that should help to keep things
+    // distributed
+    Vector2 RandomPosition(Vector3 lastPosition, int seed)
+    {
+        return "yes";
+    }
+}
